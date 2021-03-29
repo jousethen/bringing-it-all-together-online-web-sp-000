2328e75c767d392cdfdde80536438280252a3dd1
Jouse then <jthen93@gmail.com>
2021-03-29 15:26:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,6 +20,14 @@ class Dog
     end
   end
   
+  def update
+    sql = <<-SQL
+        UPDATE dogs
+        SET name = ?, breed = ? WHERE id = ?
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
   
   def self.create_table
     sql = <<-SQL
