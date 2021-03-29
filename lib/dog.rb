40eb2be6f2b04dddcd5292fe5f6a4df8429bfa4f
Jouse then <jthen93@gmail.com>
2021-03-29 15:25:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,6 +7,20 @@ class Dog
     @id = id
   end
   
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end
+  
+  
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS dogs (
@@ -22,4 +36,6 @@ class Dog
     sql = "DROP TABLE IF EXISTS dogs"
     DB[:conn].execute(sql)
   end
+  
+  
 end
